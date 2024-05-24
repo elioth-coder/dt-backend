8b48f894c37ccf942113e98120ac623a12176465
Elioth Coder <elioth.coder@gmail.com>
2024-05-24 11:04:58 +0800

    Final edits

--- app/Views/index.php
+++ app/Views/index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document Tracking</title>
+    <title>NEUST SYSTEM</title>
     <script src="/src/config.js"></script>
-    <script type="module" crossorigin src="/assets/index-DJHH46wj.js"></script>
+    <script type="module" crossorigin src="/assets/index-_JHmbNUp.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/index-BuwVayNK.css">
   </head>
   <body>
