00720c530f69beba12e9bad83f12e9ff67ae0302
Elioth Coder <elioth.coder@gmail.com>
2024-07-31 16:31:07 +0800

    - fixed bug on teacher schedule print page where BSED-ENG is not supported - added program column to program courses table to fix the bug

--- app/Controllers/ProgramCourseController.php
+++ app/Controllers/ProgramCourseController.php
@@ -5,11 +5,12 @@ namespace App\Controllers;
 class ProgramCourseController extends BaseController
 {
     public $table_name = 'program_course';
-    public $columns = ['code','degree','major'];
+    public $columns = ['code','degree','major','program'];
     public $rules = [
-        'code'   => 'required|string|max_length[15]',
-        'degree' => 'required|string|max_length[120]',
-        'major'  => 'required|string|max_length[100]',
+        'code'    => 'required|string|max_length[15]',
+        'degree'  => 'required|string|max_length[120]',
+        'major'   => 'required|string|max_length[100]',
+        'program' => 'required|string|max_length[15]',
     ];
 
     public $error_messages = [
@@ -27,6 +28,11 @@ class ProgramCourseController extends BaseController
             'required'   => 'Major is required.',
             'string'     => 'Major must be a string.',
             'max_length' => 'Major must be less than or equal to 100 characters.',
+        ],
+        'program' => [
+            'required'   => 'Program is required.',
+            'string'     => 'Program must be a string.',
+            'max_length' => 'Program must be less than or equal to 15 characters.',
         ],
     ];
 }
