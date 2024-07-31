eb58050f835f31d256e4e51e9b651e758feaf6d8
Elioth Coder <elioth.coder@gmail.com>
2024-07-31 16:42:22 +0800

    updated path to js file

--- app/Views/index.php
+++ app/Views/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>NEUST SYSTEM</title>
     <script src="/src/config.js"></script>
-    <script type="module" crossorigin src="/assets/index-C7mRak6y.js"></script>
+    <script type="module" crossorigin src="/assets/index-ldBQcG8P.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/index-DMs3X73Q.css">
   </head>
   <body>
