119b7b7f4c1c530bf1e814b8d0905698b2bf54a6
Elioth Coder <elioth.coder@gmail.com>
2024-08-01 14:49:23 +0800

    - remove vpaa in the header of document - added program column in the signatories

--- app/Controllers/SignatoryController.php
+++ app/Controllers/SignatoryController.php
@@ -5,28 +5,34 @@ namespace App\Controllers;
 class SignatoryController extends BaseController
 {
     public $table_name = 'signatory';
-    public $columns = ['content', 'college', 'document'];
+    public $columns = ['content', 'college', 'document', 'program'];
     public $rules = [
-        'content'  => 'required|string|max_length[10000]',
         'college'  => 'required|string|max_length[10]',
+        'program'  => 'required|string|max_length[15]',
         'document' => 'required|string|max_length[30]',
+        'content'  => 'required|string|max_length[10000]',
     ];
 
     public $error_messages = [
-        'content' => [
-            'required'   => 'Content is required.',
-            'string'     => 'Content must be a string.',
-            'max_length' => 'Content must be less than or equal to 1000 characters.',
-        ],
         'college' => [
             'required'   => 'College is required.',
             'string'     => 'College must be a string.',
             'max_length' => 'College must be less than or equal to 10 characters.',
         ],
+        'program' => [
+            'required'   => 'Program is required.',
+            'string'     => 'Program must be a string.',
+            'max_length' => 'Program must be less than or equal to 10 characters.',
+        ],
         'document' => [
             'required'   => 'Document is required.',
             'string'     => 'Document must be a string.',
             'max_length' => 'Document must be less than or equal to 30 characters.',
+        ],
+        'content' => [
+            'required'   => 'Content is required.',
+            'string'     => 'Content must be a string.',
+            'max_length' => 'Content must be less than or equal to 1000 characters.',
         ],
     ];
 }
--- app/Views/index.php
+++ app/Views/index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>NEUST SYSTEM</title>
     <script src="/src/config.js"></script>
-    <script type="module" crossorigin src="/assets/index-ldBQcG8P.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-DMs3X73Q.css">
+    <script type="module" crossorigin src="/assets/index-_DteP85s.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-B5nn6jQe.css">
   </head>
   <body>
     <div id="app"></div>
