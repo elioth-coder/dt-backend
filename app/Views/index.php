d62769f2806245abaa69544eb9d043acee6e32fc
elioth-coder <65105970+elioth-coder@users.noreply.github.com>
2025-01-02 22:46:08 +0800

    updated the frontend

--- app/Views/index.php
+++ app/Views/index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>NEUST SYSTEM</title>
     <script src="/src/config.js"></script>
-    <script type="module" crossorigin src="/assets/index-_DteP85s.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-B5nn6jQe.css">
+    <script type="module" crossorigin src="/assets/index-C8lIgyfc.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-zU9ztZSi.css">
   </head>
   <body>
     <div id="app"></div>
